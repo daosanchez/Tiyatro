fc555caf795fe5b4c7f4d15fb08384909e10cf99
sanchez <145916188+ravzasanchez@users.noreply.github.com>
2023-12-19 10:45:37 +0300

    Add files via upload

---
+++ devlet tiyoatrosu/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace devlet_tiyoatrosu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.cmbSahne = new System.Windows.Forms.ComboBox();
+            this.dtpTarih = new System.Windows.Forms.DateTimePicker();
+            this.dtvBilgi = new System.Windows.Forms.DataGridView();
+            this.lblSahne = new System.Windows.Forms.Label();
+            this.lblTarih = new System.Windows.Forms.Label();
+            this.lblSure = new System.Windows.Forms.Label();
+            this.lblOyun = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.cmbOyun = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.cbMuzikal = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtvBilgi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSil
+            // 
+            this.btnSil.Font = new System.Drawing.Font("Agency FB", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSil.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSil.Location = new System.Drawing.Point(37, 395);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Padding = new System.Windows.Forms.Padding(0, 0, 12, 0);
+            this.btnSil.Size = new System.Drawing.Size(126, 33);
+            this.btnSil.TabIndex = 44;
+            this.btnSil.Text = "SİL";
+            this.btnSil.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Font = new System.Drawing.Font("Agency FB", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuncelle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGuncelle.Location = new System.Drawing.Point(37, 352);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Padding = new System.Windows.Forms.Padding(0, 0, 12, 0);
+            this.btnGuncelle.Size = new System.Drawing.Size(126, 37);
+            this.btnGuncelle.TabIndex = 43;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Agency FB", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEkle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEkle.Location = new System.Drawing.Point(37, 313);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Padding = new System.Windows.Forms.Padding(0, 0, 12, 0);
+            this.btnEkle.Size = new System.Drawing.Size(126, 33);
+            this.btnEkle.TabIndex = 42;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // cmbSahne
+            // 
+            this.cmbSahne.Font = new System.Drawing.Font("Agency FB", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSahne.FormattingEnabled = true;
+            this.cmbSahne.Items.AddRange(new object[] {
+            "Aksiyon ",
+            "Macera ",
+            "Romantik",
+            "Komedi ",
+            "Suç/Polisiye ",
+            "Belgesel",
+            "Fantezi ",
+            "Korku ",
+            "Bilim Kurgu",
+            "Western ",
+            "Müzikal ",
+            "Tarihi "});
+            this.cmbSahne.Location = new System.Drawing.Point(56, 64);
+            this.cmbSahne.Name = "cmbSahne";
+            this.cmbSahne.Size = new System.Drawing.Size(121, 33);
+            this.cmbSahne.TabIndex = 41;
+            // 
+            // dtpTarih
+            // 
+            this.dtpTarih.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpTarih.Location = new System.Drawing.Point(12, 244);
+            this.dtpTarih.Name = "dtpTarih";
+            this.dtpTarih.Size = new System.Drawing.Size(221, 33);
+            this.dtpTarih.TabIndex = 37;
+            // 
+            // dtvBilgi
+            // 
+            this.dtvBilgi.BackgroundColor = System.Drawing.Color.DimGray;
+            this.dtvBilgi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtvBilgi.GridColor = System.Drawing.Color.Goldenrod;
+            this.dtvBilgi.Location = new System.Drawing.Point(239, 12);
+            this.dtvBilgi.Name = "dtvBilgi";
+            this.dtvBilgi.Size = new System.Drawing.Size(390, 360);
+            this.dtvBilgi.TabIndex = 36;
+            this.dtvBilgi.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtvBilgi_CellContentClick);
+            // 
+            // lblSahne
+            // 
+            this.lblSahne.AutoSize = true;
+            this.lblSahne.BackColor = System.Drawing.Color.White;
+            this.lblSahne.Font = new System.Drawing.Font("Agency FB", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSahne.Location = new System.Drawing.Point(-1, 64);
+            this.lblSahne.Name = "lblSahne";
+            this.lblSahne.Size = new System.Drawing.Size(58, 25);
+            this.lblSahne.TabIndex = 34;
+            this.lblSahne.Text = "SAHNE:";
+            // 
+            // lblTarih
+            // 
+            this.lblTarih.AutoSize = true;
+            this.lblTarih.BackColor = System.Drawing.Color.White;
+            this.lblTarih.Font = new System.Drawing.Font("Agency FB", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTarih.Location = new System.Drawing.Point(67, 205);
+            this.lblTarih.Name = "lblTarih";
+            this.lblTarih.Size = new System.Drawing.Size(110, 25);
+            this.lblTarih.TabIndex = 33;
+            this.lblTarih.Text = "İZLEME TARİHİ:";
+            // 
+            // lblSure
+            // 
+            this.lblSure.AutoSize = true;
+            this.lblSure.BackColor = System.Drawing.Color.White;
+            this.lblSure.Font = new System.Drawing.Font("Agency FB", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSure.Location = new System.Drawing.Point(0, 155);
+            this.lblSure.Name = "lblSure";
+            this.lblSure.Size = new System.Drawing.Size(50, 25);
+            this.lblSure.TabIndex = 32;
+            this.lblSure.Text = "SÜRE:";
+            // 
+            // lblOyun
+            // 
+            this.lblOyun.AutoSize = true;
+            this.lblOyun.BackColor = System.Drawing.Color.White;
+            this.lblOyun.Font = new System.Drawing.Font("Agency FB", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOyun.Location = new System.Drawing.Point(7, 19);
+            this.lblOyun.Name = "lblOyun";
+            this.lblOyun.Size = new System.Drawing.Size(48, 25);
+            this.lblOyun.TabIndex = 31;
+            this.lblOyun.Text = "Oyun:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(57, 160);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 45;
+            // 
+            // cmbOyun
+            // 
+            this.cmbOyun.Font = new System.Drawing.Font("Agency FB", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbOyun.FormattingEnabled = true;
+            this.cmbOyun.Items.AddRange(new object[] {
+            "Aksiyon ",
+            "Macera ",
+            "Romantik",
+            "Komedi ",
+            "Suç/Polisiye ",
+            "Belgesel",
+            "Fantezi ",
+            "Korku ",
+            "Bilim Kurgu",
+            "Western ",
+            "Müzikal ",
+            "Tarihi "});
+            this.cmbOyun.Location = new System.Drawing.Point(56, 11);
+            this.cmbOyun.Name = "cmbOyun";
+            this.cmbOyun.Size = new System.Drawing.Size(121, 33);
+            this.cmbOyun.TabIndex = 46;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-1, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 25);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "FİYAT:";
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(56, 103);
+            this.txtFiyat.Multiline = true;
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(121, 33);
+            this.txtFiyat.TabIndex = 48;
+            // 
+            // cbMuzikal
+            // 
+            this.cbMuzikal.AutoSize = true;
+            this.cbMuzikal.BackColor = System.Drawing.Color.White;
+            this.cbMuzikal.Font = new System.Drawing.Font("Agency FB", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMuzikal.Location = new System.Drawing.Point(542, 398);
+            this.cbMuzikal.Name = "cbMuzikal";
+            this.cbMuzikal.Size = new System.Drawing.Size(87, 29);
+            this.cbMuzikal.TabIndex = 49;
+            this.cbMuzikal.Text = "MÜZİKAL";
+            this.cbMuzikal.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(684, 450);
+            this.Controls.Add(this.cbMuzikal);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbOyun);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.cmbSahne);
+            this.Controls.Add(this.dtpTarih);
+            this.Controls.Add(this.dtvBilgi);
+            this.Controls.Add(this.lblSahne);
+            this.Controls.Add(this.lblTarih);
+            this.Controls.Add(this.lblSure);
+            this.Controls.Add(this.lblOyun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtvBilgi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ComboBox cmbSahne;
+        private System.Windows.Forms.DateTimePicker dtpTarih;
+        private System.Windows.Forms.DataGridView dtvBilgi;
+        private System.Windows.Forms.Label lblSahne;
+        private System.Windows.Forms.Label lblTarih;
+        private System.Windows.Forms.Label lblSure;
+        private System.Windows.Forms.Label lblOyun;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox cmbOyun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.CheckBox cbMuzikal;
+    }
+}
+
---
+++ devlet tiyoatrosu/tiyatro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devlet_tiyoatrosu
+{
+    internal class Tiyatro
+    {
+        public string Oyun { get; set; }
+        public string Sahne { get; set; }
+        public DateTime KayitTarih { get; set; }
+        public int Sure { get; set; }
+        public int Fiyat { get; set; }
+        public bool Muzikal { get; set; }
+
+        public Tiyatro(string oyun, string sahne, DateTime kayitTarih, int sure, int fiyat, bool muzikal)
+        {
+            Oyun = oyun;
+            Sahne = sahne;
+            KayitTarih = kayitTarih;
+            Sure = sure;
+            Fiyat = fiyat;
+            Muzikal = muzikal;
+        }
+    }
+    
+}
